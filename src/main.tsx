cf61615e34365ebfd806d1ba1861192149bec219
Skylooker <huangchenglin1997@qq.com>
2022-03-29 21:54:47 +0800

    fix: 🐛 去除不必要的依赖
    BREAKING CHANGE: 🧨 main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "src/App";
-import "virtual:windi.css";
 import { StoreContext, store } from "src/stores/rootStore";
 
 ReactDOM.render(
